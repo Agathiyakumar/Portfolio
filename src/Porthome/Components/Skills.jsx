2388a63cebb78bfd06f8b72ec80bc4647cb35f7f
Agathiyakumar <harishagathi@gmail.com>
2025-01-21 16:06:48 +0530

    first

--- src/Porthome/Components/Skills.jsx
+++ src/Porthome/Components/Skills.jsx
@@ -14,7 +14,7 @@ const Skills=()=>{
         <div class="slider" style={{'--quantity': 7}}>
             <div class="item" style={{'--position': 1}}><img src="https://static-00.iconduck.com/assets.00/react-icon-512x512-u6e60ayf.png" alt=""/></div>
             <div class="item" style={{'--position': 2}}><img src="https://www.w3schools.com/whatis/img_css.jpg" alt=""/></div>
-            <div class="item" style={{'--position': 3}}><img src="https://play-lh.googleusercontent.com/rfWOJQVBHoAZ_B43v0ySFlLmJBLtksVGAxGaFRh2ex4nOmNQ86qzG4sYWV63IKrXlvI" alt=""/></div>
+            <div class="item" style={{'--position': 3}}><img src="https://skillforge.com/wp-content/uploads/2020/10/javascript.png" alt=""/></div>
             <div class="item" style={{'--position': 4}}><img src="https://www.opc-router.de/wp-content/uploads/2021/03/mongodb_thumbnail.png" alt=""/></div>
             <div class="item" style={{'--position': 5}}><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTt5943IuioBPR2G35qu1CvX7tBJ_yvlZ6J7w&s" alt=""/></div>
             <div class="item" style={{'--position': 6}}><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQEc9A_S6BPxCDRp5WjMFEfXrpCu1ya2OO-Lw&s" alt=""/></div>
